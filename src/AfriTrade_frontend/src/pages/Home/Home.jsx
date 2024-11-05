ade884badd6181987598a44a67634614c33d3f06
git_BrianWahungu <cepteinhawks@gmail.com>
2024-11-05 14:05:48 +0300

    front end done

--- src/AfriTrade_frontend/src/pages/Home/Home.jsx
+++ src/AfriTrade_frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,26 @@
 import React, { useContext, useEffect, useState } from 'react'
 import './Home.scss'
 import { CoinContext } from '../../context/CoinContext'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
 const {allCoin, currency} = useContext(CoinContext);
 const [displayCoin, setDisplayCoin] = useState([]);
+const [input, setInput] = useState('');
+const inputHandler = (event)=>{
+    setInput(event.target.value);
+    if (event.target.value === "") {
+        setDisplayCoin(allCoin);
+    }
+}
+const searchHandler = async (event) =>{
+    event.preventDefault();
+    const coins = await allCoin.filter((item)=>{
+        return item.name.toLowerCase().includes(input.toLowerCase())
+    })
+    setDisplayCoin(coins);
+}
 
 useEffect(()=>{
   setDisplayCoin(allCoin);
@@ -19,8 +34,20 @@ useEffect(()=>{
                 Sign Up to get updates about what is going on in the 
                 financial markets and improve your financial position.
             </p>
-            <form>
-                <input type="text" placeholder='Search Crypto'/>
+            <form onSubmit={searchHandler}>
+
+
+                <input onChange={inputHandler} list='coinlist' value={input} type="text" 
+                placeholder='Search Crypto' required/>
+
+                <datalist id='coinlist'>
+                    {allCoin.map((item,index)=>(<option key={index} value={item.name}/>))}
+                </datalist>
+
+
+
+
+
                 <button type="submit">Search</button>
             </form>
         </div>
@@ -34,7 +61,7 @@ useEffect(()=>{
             </div>
             {
                 displayCoin.slice(0,10).map((item, index)=>(
-                    <div className="table-layout" key={index}>
+                    <Link to={`/coin/${item.id}`} className="table-layout" key={index}>
                         <p>{item.market_cap_rank}</p>
                         <div>
                             <img src={item.image} alt="" />
@@ -45,7 +72,7 @@ useEffect(()=>{
                             {Math.floor(item.price_change_percentage_24h*100)/100}
                         </p>
                         <p className='market-cap'>{currency.symbol} {item.market_cap.toLocaleString()}</p>
-                    </div>
+                    </Link>
                 ))
             }
         </div>
